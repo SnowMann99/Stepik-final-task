a233059ed7f571b408de953a8d2b045a6fc98e77
thereisnowayy <kontyolo34np@gmail.com>
2020-03-12 14:05:00 +0500

    little fix browser_name values

--- conftest.py
+++ conftest.py
@@ -4,14 +4,14 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-     parser.addoption('--browser_name', action='store', default=None,
+     parser.addoption('--browser_name', action='store', default='chrome',
                       help="Choose browser: chrome or firefox")
      parser.addoption('--language', action='store', default='en',
                      help="Choose language: ru, en, es, fr")
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = "firefox"
+    browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
     if browser_name == "chrome":
         options = Options()
