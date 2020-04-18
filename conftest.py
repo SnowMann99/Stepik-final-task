9cb5bd17365202bc86d3f5a87fb2df057e0ec1b8
Thereisnowayyy <kontyolo34np@gmail.com>
2020-04-18 18:46:23 +0500

    conftest.py in Pep-8 style

--- conftest.py
+++ conftest.py
@@ -4,10 +4,11 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-     parser.addoption('--browser_name', action='store', default='chrome',
-                      help="Choose browser: chrome or firefox")
-     parser.addoption('--language', action='store', default='en',
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
                      help="Choose language: ru, en, es, fr")
+
 
 @pytest.fixture(scope="function")
 def browser(request):
